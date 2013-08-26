266e922d5423a0c1be67e8883ca9dcc511350980
David Burinov <burinov@gmail.com>
2013-08-26 19:25:02 +0400

    Web scraping works for Python 2.7

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from urllib.request import urlopen
-import sys
+#import bs4
 from bs4 import BeautifulSoup
-import re
-import codecs
+from urllib2 import urlopen
 
-#Getting page source
-def getPageSource(url):
-    page = urlopen(url)
-    html = page.read()
-    return BeautifulSoup(html)
+url = "http://www.sports.ru/tags/1046171.html?type=news"
+source = urlopen(url)
 
-if __name__ == "__main__":
-    url = "http://www.sports.ru/tags/1046171.html?type=news"
-    soup = getPageSource(url).prettify()
+content = source.read() 
+source.close()
 
-    print(soup.encode('utf-8'))
+print content
+from urllib2 import urlopen
+
+url = "http://www.sports.ru/tags/1046171.html?type=news"
+source = urlopen(url)
+
+content = source.read() 
+source.close()
+
+print content
